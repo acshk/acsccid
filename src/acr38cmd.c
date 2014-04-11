8b34e83ad6fcdf12c6ef8bfa51f06cb23e58eed2
Godfrey Chung <godfrey.chung@acs.com.hk>
2014-04-11 18:19:29 +0800

    Remove tx_length checking in ACR38_TransmitPPS().

--- src/acr38cmd.c
+++ src/acr38cmd.c
@@ -395,13 +395,6 @@ RESPONSECODE ACR38_TransmitPPS(unsigned int reader_index, unsigned int tx_length
 	status_t ret;
 	_ccid_descriptor *ccid_descriptor = get_ccid_descriptor(reader_index);
 
-	/* check that the command is not too large */
-	if (tx_length > CMD_BUF_SIZE)
-	{
-		DEBUG_CRITICAL2("TX Length too big: %d", tx_length);
-		return IFD_NOT_SUPPORTED;
-	}
-
 	// SET_CARD_PPS or SET_SAM_CARD_PPS
 	cmd[0] = 0x01;
 	cmd[1] = ccid_descriptor->bCurrentSlotIndex == 0 ? 0x0A : 0x0C;
