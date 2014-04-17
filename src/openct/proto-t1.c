f6d5b5a1687b46fb67ac7853c8a9157dc1baa32d
Godfrey Chung <godfrey.chung@acs.com.hk>
2014-04-17 12:03:29 +0800

    Remove the redundant rmax assignment in t1_xcv().

--- src/openct/proto-t1.c
+++ src/openct/proto-t1.c
@@ -686,7 +686,6 @@ static int t1_xcv(t1_state_t * t1, unsigned char *block, size_t slen,
 		 * be the same on 64-bits architectures for example (iMac G5) */
 		rmax_int = rmax;
 		n = ccid_slot->pReceive(t1 -> lun, &rmax_int, block, NULL);
-		rmax = rmax_int;
 
 		if (n == IFD_PARITY_ERROR)
 			return -2;
