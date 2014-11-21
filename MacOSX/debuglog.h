5d7c1c0d2f3cf314412ce050606ccfb84cfc8331
Godfrey Chung <godfrey.chung@acs.com.hk>
2014-11-21 17:45:21 +0800

    Import MacOSX/debuglog.h from ccid 1.4.18.

--- MacOSX/debuglog.h
+++ MacOSX/debuglog.h
@@ -1,12 +1,38 @@
 /*
- * MUSCLE SmartCard Development ( http://www.linuxnet.com )
+ * MUSCLE SmartCard Development ( http://pcsclite.alioth.debian.org/pcsclite.html )
  *
  * Copyright (C) 1999-2004
- *  David Corcoran <corcoran@linuxnet.com>
- * Copyright (C) 1999-2005
+ *  David Corcoran <corcoran@musclecard.com>
+ * Copyright (C) 1999-2011
  *  Ludovic Rousseau <ludovic.rousseau@free.fr>
  *
- * $Id: debuglog.h 3075 2008-07-30 14:25:27Z rousseau $
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+Changes to this license can be made only by the copyright author with
+explicit written consent.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: debuglog.h 6851 2014-02-14 15:43:32Z rousseau $
  */
 
 /**
@@ -33,22 +59,15 @@
 #ifndef __debuglog_h__
 #define __debuglog_h__
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 #ifndef PCSC_API
 #define PCSC_API
 #endif
 
-#define DEBUGLOG_LOG_ENTRIES    1
-#define DEBUGLOG_IGNORE_ENTRIES 2
-
 enum {
 	DEBUGLOG_NO_DEBUG = 0,
 	DEBUGLOG_SYSLOG_DEBUG,
-	DEBUGLOG_STDERR_DEBUG
+	DEBUGLOG_STDOUT_DEBUG,
+	DEBUGLOG_STDOUT_COLOR_DEBUG
 };
 
 #define DEBUG_CATEGORY_NOTHING  0
@@ -67,11 +86,33 @@ enum {
 #define __FUNCTION__ ""
 #endif
 
+#ifndef __GNUC__
+#define __attribute__(x) /*nothing*/
+#endif
+
+#ifdef NO_LOG
+
+#define Log0(priority) do { } while(0)
+#define Log1(priority, fmt) do { } while(0)
+#define Log2(priority, fmt, data) do { } while(0)
+#define Log3(priority, fmt, data1, data2) do { } while(0)
+#define Log4(priority, fmt, data1, data2, data3) do { } while(0)
+#define Log5(priority, fmt, data1, data2, data3, data4) do { } while(0)
+#define Log9(priority, fmt, data1, data2, data3, data4, data5, data6, data7, data8) do { } while(0)
+#define LogXxd(priority, msg, buffer, size) do { } while(0)
+
+#define DebugLogA(a) 
+#define DebugLogB(a, b) 
+#define DebugLogC(a, b,c) 
+
+#else
+
 #define Log0(priority) log_msg(priority, "%s:%d:%s()", __FILE__, __LINE__, __FUNCTION__)
 #define Log1(priority, fmt) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__)
 #define Log2(priority, fmt, data) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data)
 #define Log3(priority, fmt, data1, data2) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2)
 #define Log4(priority, fmt, data1, data2, data3) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2, data3)
+#define Log5(priority, fmt, data1, data2, data3, data4) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2, data3, data4)
 #define Log9(priority, fmt, data1, data2, data3, data4, data5, data6, data7, data8) log_msg(priority, "%s:%d:%s() " fmt, __FILE__, __LINE__, __FUNCTION__, data1, data2, data3, data4, data5, data6, data7, data8)
 #define LogXxd(priority, msg, buffer, size) log_xxd(priority, msg, buffer, size)
 
@@ -79,7 +120,11 @@ enum {
 #define DebugLogB(a, b) Log2(PCSC_LOG_INFO, a, b)
 #define DebugLogC(a, b,c) Log3(PCSC_LOG_INFO, a, b, c)
 
-PCSC_API void log_msg(const int priority, const char *fmt, ...);
+#endif	/* NO_LOG */
+
+PCSC_API void log_msg(const int priority, const char *fmt, ...)
+	__attribute__((format(printf, 2, 3)));
+
 PCSC_API void log_xxd(const int priority, const char *msg,
 	const unsigned char *buffer, const int size);
 
@@ -88,10 +133,6 @@ void DebugLogSetLogType(const int);
 int DebugLogSetCategory(const int);
 void DebugLogCategory(const int, const unsigned char *, const int);
 PCSC_API void DebugLogSetLevel(const int level);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif							/* __debuglog_h__ */
 
