fc6695b7dd581433a72b243c4c760824e1fd634c
Godfrey Chung <godfrey.chung@acs.com.hk>
2014-11-25 14:29:59 +0800

    Merge the macros in src/ccid_usb.c from ccid 1.4.18.
    USB_WRITE_TIMEOUT and ALLOW_PROPRIETARY_CLASS.

--- src/ccid_usb.c
+++ src/ccid_usb.c
@@ -48,13 +48,13 @@
 
 /* write timeout
  * we don't have to wait a long time since the card was doing nothing */
-#define USB_WRITE_TIMEOUT (5 * 1000)  /* 5 seconds timeout */
+#define USB_WRITE_TIMEOUT (5 * 1000)	/* 5 seconds timeout */
 
 /*
  * Proprietary USB Class (0xFF) are (or are not) accepted
  * A proprietary class is used for devices released before the final CCID
  * specifications were ready.
- * We should not have problems with non CCID devices becasue the
+ * We should not have problems with non CCID devices because the
  * Manufacturer and Product ID are also used to identify the device */
 #define ALLOW_PROPRIETARY_CLASS
 
