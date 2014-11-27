a460f63fd7e87665f45b43d3c972bd63f3a8342c
Godfrey Chung <godfrey.chung@acs.com.hk>
2014-11-27 14:31:53 +0800

    Merge the copyright in src/ccid_usb.c from ccid 1.4.18.

--- src/ccid_usb.c
+++ src/ccid_usb.c
@@ -1,17 +1,17 @@
 /*
-    ccid_usb.c: USB access routines using the libusb library
-    Copyright (C) 2003-2009   Ludovic Rousseau
-    Copyright (C) 2009-2014   Advanced Card Systems Ltd.
+	ccid_usb.c: USB access routines using the libusb library
+	Copyright (C) 2003-2010	Ludovic Rousseau
+	Copyright (C) 2009-2014	Advanced Card Systems Ltd.
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
 
 	You should have received a copy of the GNU Lesser General Public License
 	along with this library; if not, write to the Free Software Foundation,
@@ -19,7 +19,7 @@
 */
 
 /*
- * $Id: ccid_usb.c 4346 2009-07-28 13:39:37Z rousseau $
+ * $Id: ccid_usb.c 6975 2014-09-04 11:33:05Z rousseau $
  */
 
 #define __CCID_USB__
