b599db3107acff90dd39929e6e60756d33e2f8c4
Godfrey Chung <godfrey.chung@acs.com.hk>
2014-12-01 16:17:50 +0800

    Remove the prototypes from src/ifdhandler.c.

--- src/ifdhandler.c
+++ src/ifdhandler.c
@@ -77,10 +77,6 @@ BYTE ACR38CardVoltage = 0;
 DWORD ACR38CardType = 0;
 
 /* local functions */
-#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && defined(USE_USB_INTERRUPT)
-static RESPONSECODE IFDHPolling(DWORD Lun);
-static RESPONSECODE IFDHSleep(DWORD Lun);
-#endif
 static void init_driver(void);
 static void extra_egt(ATR_t *atr, _ccid_descriptor *ccid_desc, DWORD Protocol);
 static char find_baud_rate(unsigned int baudrate, unsigned int *list);
