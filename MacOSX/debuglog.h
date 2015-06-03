48f05c140ecf2e26fc6916d31ac98a0e33b055d5
Godfrey Chung <godfrey.chung@acs.com.hk>
2015-06-03 10:41:03 +0800

    Import MacOSX/debuglog.h from ccid 1.4.19.

--- MacOSX/debuglog.h
+++ MacOSX/debuglog.h
@@ -18,9 +18,6 @@ are met:
 3. The name of the author may not be used to endorse or promote products
    derived from this software without specific prior written permission.
 
-Changes to this license can be made only by the copyright author with
-explicit written consent.
-
 THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -32,7 +29,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: debuglog.h 6851 2014-02-14 15:43:32Z rousseau $
+ * $Id$
  */
 
 /**
@@ -101,9 +98,9 @@ enum {
 #define Log9(priority, fmt, data1, data2, data3, data4, data5, data6, data7, data8) do { } while(0)
 #define LogXxd(priority, msg, buffer, size) do { } while(0)
 
-#define DebugLogA(a) 
-#define DebugLogB(a, b) 
-#define DebugLogC(a, b,c) 
+#define DebugLogA(a)
+#define DebugLogB(a, b)
+#define DebugLogC(a, b,c)
 
 #else
 
