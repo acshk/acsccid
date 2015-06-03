fca3674ea98f45869e3cf4b39ddd32572671a4e2
Godfrey Chung <godfrey.chung@acs.com.hk>
2015-06-03 14:14:09 +0800

    Add log_level parameter to ccid_error().
    Merge from ccid 1.4.19.

--- src/ccid.h
+++ src/ccid.h
@@ -330,7 +330,8 @@ typedef struct
 
 int ccid_open_hack_pre(unsigned int reader_index);
 int ccid_open_hack_post(unsigned int reader_index);
-void ccid_error(int error, const char *file, int line, const char *function);
+void ccid_error(int log_level, int error, const char *file, int line,
+	const char *function);
 _ccid_descriptor *get_ccid_descriptor(unsigned int reader_index);
 void acr38_error(int error, const char *file, int line, const char *function);
 void EnablePicc(unsigned int reader_index, int enabled);
