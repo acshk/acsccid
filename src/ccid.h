81e7db442215f46691cb2f6c2a41ccdfdd9852be
Godfrey Chung <godfrey.chung@acs.com.hk>
2015-07-27 12:12:19 +0800

    Update the copyright in src/ccid.h.

--- src/ccid.h
+++ src/ccid.h
@@ -1,7 +1,7 @@
 /*
     ccid.h: CCID structures
     Copyright (C) 2003-2010   Ludovic Rousseau
-    Copyright (C) 2009-2014   Advanced Card Systems Ltd.
+    Copyright (C) 2009-2015   Advanced Card Systems Ltd.
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
