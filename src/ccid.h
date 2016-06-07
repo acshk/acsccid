e1f959db66ce070c66d22bb7e19111b56037ed8b
Godfrey Chung <godfrey.chung@acs.com.hk>
2016-06-07 11:55:28 +0800

    SCM SCL011: use a 400 ms timeout instead of 100 ms.
    Merge from ccid 1.4.24.

--- src/ccid.h
+++ src/ccid.h
@@ -245,6 +245,7 @@ typedef struct
 #define MICROCHIP_SEC1100 0x04241104
 #define CHERRY_KC1000SC 0x046A00A1
 #define ElatecTWN4	0x09D80427
+#define SCM_SCL011 0x04E65293
 
 // CCID readers
 #define ACS_ACR32_ICC_READER			0x072fb301
