672ca7a1d7d51429f1055cf3ed15b4dd1b918858
Godfrey Chung <godfrey.chung@acs.com.hk>
2017-10-19 17:54:08 +0800

    Enable specific I/O controls for APG8201-B2 in IFDHControl().

--- src/ifdhandler.c
+++ src/ifdhandler.c
@@ -2493,6 +2493,7 @@ EXTERNAL RESPONSECODE IFDHControl(DWORD Lun, DWORD dwControlCode,
 	if ((ACS_ACR83U == ccid_descriptor -> readerID) ||
 		(ACS_ACR85_PINPAD_READER_ICC == ccid_descriptor -> readerID) ||
 		(ACS_APG8201 == ccid_descriptor -> readerID) ||
+		(ACS_APG8201_B2 == ccid_descriptor -> readerID) ||
 		(ACS_APG8201Z == ccid_descriptor -> readerID) ||
 		(ACS_APG8201Z2 == ccid_descriptor -> readerID))
 	{
